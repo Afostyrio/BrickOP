5e1fc8bdd0ec0b16eb5aed4738773a3840478c53
Afostyrio <87393120+Afostyrio@users.noreply.github.com>
2021-12-14 21:56:38 -0600

    Update pemdas.h
    It now contains the `solveLineByLine()` function, capable of solving full PEMDAS files.

--- lib/pemdas.h
+++ lib/pemdas.h
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<math.h>
 #include "printer.h"
 
@@ -9,10 +10,14 @@
 #define MAX_FILENAME_SIZE 255
 #define MAX_OPERATION_SIZE 1000
 
-FILE* getLineFromFile(char filename[MAX_FILENAME_SIZE]);
+//FILE* getLineFromFile(char filename[MAX_FILENAME_SIZE]);
+void solveLineByLine(char filename[MAX_FILENAME_SIZE]);
 int countNumbers(FILE* stream);
-int countOperators(FILE* stream);
-double pemdasSolve(FILE* stream);
+//int countOperators(FILE* stream);
+double pemdasSolve(char filename[MAX_FILENAME_SIZE]);
 double sumaVector(double* vec, int size);
+void removeNewline(char* str);
+void removeNulChar(char* str);
+void clearParen(char filename[MAX_FILENAME_SIZE]);
 
 #endif

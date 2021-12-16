e0744ab9f57cca9655ebc1f2541b4fc7afd2fb64
Afostyrio <87393120+Afostyrio@users.noreply.github.com>
2021-12-16 00:14:30 -0600

    Cleanup and new prototypes

--- lib/pemdas.h
+++ lib/pemdas.h
@@ -10,14 +10,10 @@
 #define MAX_FILENAME_SIZE 255
 #define MAX_OPERATION_SIZE 1000
 
-//FILE* getLineFromFile(char filename[MAX_FILENAME_SIZE]);
 void solveLineByLine(char filename[MAX_FILENAME_SIZE]);
 int countNumbers(FILE* stream);
-//int countOperators(FILE* stream);
-double pemdasSolve(char filename[MAX_FILENAME_SIZE]);
+double pemdasSolve(FILE* stream, FILE* destination);
 double sumaVector(double* vec, int size);
-void removeNewline(char* str);
-void removeNulChar(char* str);
-void clearParen(char filename[MAX_FILENAME_SIZE]);
+void clearParen(char filename[MAX_FILENAME_SIZE], FILE* destination);
 
 #endif

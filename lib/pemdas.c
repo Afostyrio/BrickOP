f53c400df47aa989ac867496f2d6b2077800605e
Afostyrio <87393120+Afostyrio@users.noreply.github.com>
2021-12-17 02:47:27 -0600

    Correct deletion of auxiliary files.

--- lib/pemdas.c
+++ lib/pemdas.c
@@ -4,6 +4,7 @@ void clearParen(char filename[MAX_FILENAME_SIZE], FILE* destination){
 	FILE* stream = fopen(filename, "rb+");
 	char* str = malloc(MAX_OPERATION_SIZE * sizeof(char));
 	fgets(str, MAX_OPERATION_SIZE, stream);
+	fclose(stream);
 	int i = 0, parenFirstPos = -1;
 	while(str[i]){	
 		if(str[i] == '('){
@@ -13,6 +14,8 @@ void clearParen(char filename[MAX_FILENAME_SIZE], FILE* destination){
 	}
 	
 	if(parenFirstPos < 0){
+		free(str);
+		fclose(stream);
 		return;
 	}
 	
@@ -29,15 +32,13 @@ void clearParen(char filename[MAX_FILENAME_SIZE], FILE* destination){
 	}
 	substr[parenLastPos-parenFirstPos-1] = '\n';
 	substr[parenLastPos-parenFirstPos] = 0;
-	FILE* subOperation = fopen("subOp.txt", "w+");
+	FILE* subOperation = fopen("subOp.bin", "wb+");
 	fputs(substr, subOperation);
 	free(substr);
 	fclose(subOperation);
-	subOperation = fopen("subOp.txt", "r+");
+	subOperation = fopen("subOp.bin", "rb+");
 	double result = pemdasSolve(subOperation, subOperation);
-	fclose(stream);
 	rewind(subOperation);
-	remove("subOp.txt");
 	
 	char* head = malloc(MAX_OPERATION_SIZE * sizeof(char));
 	char* subStep = malloc(MAX_OPERATION_SIZE * sizeof(char));
@@ -60,20 +61,26 @@ void clearParen(char filename[MAX_FILENAME_SIZE], FILE* destination){
 	while(fgets(subStep, MAX_OPERATION_SIZE, subOperation) != NULL){
 		fputs(head, destination);
 		fprintf(destination, "(");
+
 		fputs(subStep, destination);
 		fseek(destination, -2, SEEK_END);
 		fprintf(destination, ")");
 		
 		fputs(tail, destination);
-		fseek(destination, -2, SEEK_END);
 	}
+	fclose(subOperation);
+	remove("subOp.bin");
+	
+	fputs(head, destination);
+	fprintf(destination, "%lf", result);
+	fputs(tail, destination);
 
 	stream = fopen(filename, "wb");
 	fputs(head, stream);
 	fprintf(stream, "%lf", result);
 	fputs(tail, stream);
 	fclose(stream);
-	free(str);
+	free(str); free(subStep);
 	free(head); free(tail);
 	
 	clearParen(filename, destination);
@@ -82,27 +89,27 @@ void clearParen(char filename[MAX_FILENAME_SIZE], FILE* destination){
 void solveLineByLine(char filename[MAX_FILENAME_SIZE]){
 	FILE* whole = fopen(filename, "r");
 	if(!whole){
-		printf("Error de archivo.");
+		printf("Error de archivo."); return;
 	}
-	FILE* results = fopen("results.txt", "w");
+	FILE* results = fopen("resultados.txt", "w");
 	if(!results){
-		printf("Error");
+		printf("Error en creacion de archivo de resultados"); return;
 	}
 
 	while(!feof(whole)){
 		char* str = malloc(MAX_OPERATION_SIZE * sizeof(char));
 		fgets(str, MAX_OPERATION_SIZE, whole);
-		FILE* oneLine = fopen("auxfile.txt", "w");
+		FILE* oneLine = fopen("oneline.bin", "wb");
 		fputs(str, oneLine);
 		fclose(oneLine);
 		fputs(str, results);
 		free(str);
-		clearParen("auxfile.txt", results);
-		oneLine = fopen("auxfile.txt", "r+");
+		clearParen("oneline.bin", results);
+		oneLine = fopen("oneline.bin", "rb+");
 		pemdasSolve(oneLine, results);
 		fclose(oneLine);
 		fprintf(results, "\n");
-		remove("auxfile.txt");
+		remove("oneline.bin");
 	}
 	fclose(results);
 	fclose(whole);
